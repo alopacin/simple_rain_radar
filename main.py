1b97ab5f35b37874a7828590da5620ad6adcbe7a
alopacin <adlopacinski@gmail.com>
2023-06-21 20:30:34 +0200

    dalsza praca nad programem

--- main.py
+++ main.py
@@ -2,27 +2,35 @@ import requests
 import datetime
 import json
 
+
+def check_rain_forecast() :
+    if data[0] > 0.0:
+        return 'Będzie padać'
+    elif data[0] == 0.0:
+        return 'Nie będzie padać'
+    else:
+        return'Nie wiem'
+
+
 f = 'history.txt'
 latitude = "51.51"
 longitude = "-0.13"
 tomorrow = datetime.date.today() + datetime.timedelta(days = 1)
 formatted_tomorrow = tomorrow.strftime('%Y-%m-%d')
-ask = input('Podaj we formacie YYYY-mm-dd jaką datę chcesz sprawdzić: ')
+requested_date = input('Podaj we formacie YYYY-mm-dd jaką datę chcesz sprawdzić: ')
 
-if ask == '':
-    url = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&"
-                       f"hourly=rain&daily=rain_sum&timezone=Europe%2FLondon&start_date={formatted_tomorrow}&end_date={formatted_tomorrow}")
-    data = url.json()['daily']['rain_sum']
-    print(url.status_code)
-else:
-    url = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude{longitude}&"
-                       f"hourly=rain&daily=rain_sum&timezone=Europe%2FLondon&start_date={ask}&end_date={ask}")
-    data = url.json()['daily']['rain_sum']
-    print(url.status_code)
+if requested_date == '' :
+    requested_date = formatted_tomorrow
+
+url = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&"
+                   f"hourly=rain&daily=rain_sum&timezone=Europe%2FLondon&start_date={requested_date}&end_date={requested_date}")
+data = url.json()['daily']['rain_sum']
+print(url.status_code)
+print(check_rain_forecast())
+
 
 print(data)
-with open(f, 'w') as f:
-    json.dump(data, f)
+with open(f, 'w') as file:
+    json.dump(data, file)
 
-print(tomorrow)
-print(ask)
+print(requested_date)
